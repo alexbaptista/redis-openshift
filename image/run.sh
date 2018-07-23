57fc914a7cad0a920d2642850841c96570ea88b2
Alex Caio dos Santos Baptista <alex.baptista@concrete.com.br>
2018-07-22 23:29:28 -0300

    FIX redis password

--- image/run.sh
+++ image/run.sh
@@ -14,6 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+echo "requirepass \"${REDIS_PASSWORD}\"" >> /redis-master/redis.conf
+echo "requirepass \"${REDIS_PASSWORD}\"" >> /redis-slave/redis.conf
+
 function launchmaster() {
   if [[ ! -e /redis-master-data ]]; then
     echo "Redis master data doesn't exist, data won't be persistent!"
